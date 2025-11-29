03b4510262e8ae0f491d17e74526e3a0b2af858c
almor <lifergamerus@gmail.com>
2025-11-29 06:39:57 +0500

    test

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -1,25 +1,9 @@
 package logger
 
 import (
-	"bytes"
-	"encoding/json"
 	"log"
-	"net/http"
 	"time"
 )
-
-type SeqConfig struct {
-	URL     string
-	APIKey  string
-	Enabled bool
-}
-
-var seqCfg SeqConfig
-
-func InitSeq(cfg SeqConfig) {
-	seqCfg = cfg
-	log.Printf("[INFO] %d | Seq logging enabled: %v", time.Now().Unix(), cfg.Enabled)
-}
 
 func Info(msg string) {
 	writeLog("INFO", msg, nil)
@@ -37,28 +21,4 @@ func writeLog(level, msg string, err error) {
 	} else {
 		log.Printf("[%s] %d | %s", level, now, msg)
 	}
-
-	if seqCfg.Enabled {
-		go sendToSeq(level, msg, err)
-	}
-}
-
-func sendToSeq(level, msg string, err error) {
-	body := map[string]interface{}{
-		"@l":      level,
-		"@m":      msg,
-		"unix_ts": time.Now().Unix(),
-		"@t":      time.Now().Format(time.RFC3339),
-	}
-	if err != nil {
-		body["@x"] = err.Error()
-	}
-
-	j, _ := json.Marshal(body)
-	req, _ := http.NewRequest("POST", seqCfg.URL, bytes.NewBuffer(j))
-	req.Header.Set("Content-Type", "application/json")
-	if seqCfg.APIKey != "" {
-		req.Header.Set("X-Seq-ApiKey", seqCfg.APIKey)
-	}
-	_, _ = http.DefaultClient.Do(req) // ошибки игнорируем
 }
